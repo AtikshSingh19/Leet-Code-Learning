543be0e9d2c2b3fb056ecd0dcd5fdcbc6ad4ff65
Atiksh Singh <atiksh.singhh19@gmail.com>
2026-08-14 06:47:25 +0530

    Time: 2 ms (38.30%) | Memory: 8.3 MB (98.36%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,7 +5,7 @@ public:
         int rev = 0;
         while (x!=0){
             digit = x%10;
-            if (rev>INT_MAX/10 || rev < INT_MIN/10){
+            if (rev>INT_MAX/10 || rev < INT_MIN/10){      //start me check kiya taki int rev me bada value nhi aaye kabhi taki exceed na kree and error na aaye.
                 return 0;
             }
             rev = 10*rev + digit;
